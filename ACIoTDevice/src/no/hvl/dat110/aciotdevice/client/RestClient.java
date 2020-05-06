893e07118dd9de78ae4ec801e93d3649429405a7
h579684 <42584471+h579684@users.noreply.github.com>
2020-05-06 14:14:35 +0200

    ok

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,29 +1,66 @@
 package no.hvl.dat110.aciotdevice.client;
 
-public class RestClient {
+import java.io.IOException;
 
-	public RestClient() {
-		// TODO Auto-generated constructor stub
+import com.google.gson.Gson;
+import okhttp3.*;
+
+
+	public class RestClient {
+
+		public RestClient() {
+			// TODO Auto-generated constructor stub
+		}
+
+		private static String logpath = "/accessdevice/log";
+
+		public void doPostAccessEntry(String message) {
+
+			// TODO: implement a HTTP POST on the service to post the message
+			OkHttpClient client = new OkHttpClient();
+
+			Request request = new Request.Builder()
+					.url("http://localhost:8080"+logpath)
+					.post(RequestBody.create(MediaType.parse("text/plain"),message))
+					.build();
+
+			System.out.println(request.toString());
+
+			try (Response response = client.newCall(request).execute()) {
+				System.out.println (response.body().string());
+			
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		private static String codepath = "/accessdevice/code";
+		
+		public AccessCode doGetAccessCode() {
+
+			AccessCode code = null;
+			
+			// TODO: implement a HTTP GET on the service to get current access code
+			OkHttpClient client = new OkHttpClient();
+
+			Request request = new Request.Builder()
+					.url("http://localhost:8080"+codepath)
+					.get()
+					.build();
+
+			System.out.println(request.toString());
+
+			try (Response response = client.newCall(request).execute()) {
+				Gson gson = new Gson();
+				code = gson.fromJson(response.body().string(), AccessCode.class);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return code;
+		}
 	}
 
-	private static String logpath = "/accessdevice/log";
-
-	public void doPostAccessEntry(String message) {
-
-		// TODO: implement a HTTP POST on the service to post the message
-		
-	}
-	
-	private static String codepath = "/accessdevice/code";
-	
-	public AccessCode doGetAccessCode() {
-		
-	
-
-		AccessCode code = null;
-		
-		// TODO: implement a HTTP GET on the service to get current access code
-		
-		return code;
-	}
-}
